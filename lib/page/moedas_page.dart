4fd3bf71f69983c90d8cbf82cdf448a134e649f4
Felipe Dias <adfelipealmeida@gmail.com>
2022-02-17 07:02:17 -0300

    dynamic appBar

--- lib/page/moedas_page.dart
+++ lib/page/moedas_page.dart
@@ -1,23 +1,97 @@
+import 'package:aula_1/models/moeda.dart';
 import 'package:aula_1/repositories/moeda_repository.dart';
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 
-class MoedasPage extends StatelessWidget {
+class MoedasPage extends StatefulWidget {
   const MoedasPage({Key? key}) : super(key: key);
 
   @override
-  Widget build(BuildContext context) {
-    final tabela = MoedaRepository.tabela;
+  State<MoedasPage> createState() => _MoedasPageState();
+}
 
-    return Scaffold(
-      appBar: AppBar(
+class _MoedasPageState extends State<MoedasPage> {
+  List<Moeda> selecionadas = [];
+  final tabela = MoedaRepository.tabela;
+  NumberFormat real = NumberFormat.currency(
+    locale: 'pt_BR',
+    name: 'R\$',
+  );
+
+  appBarDinamica() {
+    if (selecionadas.isEmpty) {
+      return AppBar(
         title: const Text("Cripto Moedas"),
-      ),
+      );
+    } else {
+      return AppBar(
+        title: Text(
+          selecionadas.length > 1
+              ? '${selecionadas.length} selecionadas'
+              : '${selecionadas.length} selecionada',
+        ),
+        leading: IconButton(
+          icon: const Icon(Icons.arrow_back),
+          onPressed: () {
+            setState(() {
+              selecionadas = [];
+            });
+          },
+        ),
+        backgroundColor: Colors.blueGrey[50],
+        elevation: 1, // elimina a sombra da AppBar
+        iconTheme: const IconThemeData(color: Colors.black87),
+        titleTextStyle: const TextStyle(
+          color: Colors.black87,
+          fontSize: 20,
+          fontWeight: FontWeight.bold,
+        ),
+      );
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: appBarDinamica(),
       body: ListView.separated(
         itemBuilder: (BuildContext context, int index) {
           return ListTile(
-            leading: Image.asset(tabela[index].icone),
-            title: Text(tabela[index].nome),
-            trailing: Text(tabela[index].preco.toString()),
+            shape: const RoundedRectangleBorder(
+              borderRadius: BorderRadius.all(
+                Radius.circular(16),
+              ),
+            ),
+            leading: (selecionadas.contains(tabela[index]))
+                ? const CircleAvatar(
+                    child: Icon(Icons.check),
+                  )
+                : SizedBox(
+                    child: Image.asset(tabela[index].icone),
+                    width: 40,
+                  ),
+            title: Text(
+              tabela[index].nome,
+              style: const TextStyle(
+                fontSize: 17,
+                fontWeight: FontWeight.w500,
+              ),
+            ),
+            trailing: Text(
+              real.format(tabela[index].preco),
+            ),
+            // Propriedades para quando ficar selecionado
+            selected: selecionadas.contains(tabela[index]),
+            selectedTileColor: Colors.indigo[50],
+            onLongPress: () {
+              setState(() {
+                if (selecionadas.contains(tabela[index])) {
+                  selecionadas.remove(tabela[index]);
+                } else {
+                  selecionadas.add(tabela[index]);
+                }
+              });
+            },
           );
         },
         padding: const EdgeInsets.all(16.0),
